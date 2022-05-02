2207fa9b5124c3be416717238dda13229749b5fe
subash sharma <subash.sharma@itonics.de>
2022-05-02 16:49:06 +0545

    added port

--- index.ts
+++ index.ts
@@ -6,21 +6,34 @@ const UserRouter =  require ('./src/routes/user.route');
 const RoleRouter =  require ('./src/routes/role.route');
 const BannerRouter =  require ('./src/routes/banner.route');
 const categoryRouter =  require ('./src/routes/category.route');
+const swaggerDocument = require('./src/utils/swagger.json');
 import './config/db';
+// import {swaggerDocs}  from './src/utils/swagger';
+import  swaggerUi  from 'swagger-ui-express';
 const app = express();
 
 dotenv.config();
 
+
+app.use(express.static('public'))
 app.get('/', (req: any, res: any) => {
     res.send('Welcome to Nodejs Page');
 })
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port,async () => {
     console.log('The application is listening on port 3000!');
+    // swaggerDocs(app, 3000);
 })
 
 app.use(express.urlencoded({ extended: true , limit: '50mb'}));
 app.use(express.json({limit: '50mb'}));
+
+app.use(
+    '/api-docs',
+    swaggerUi.serve, 
+    swaggerUi.setup(swaggerDocument)
+  );
 
 /**
  * Grouping Route
